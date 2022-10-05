be4a2efe9ee36661c46068742edf013ebba29509
NhorkSopheakna <99951792+NhorkSopheakna@users.noreply.github.com>
2022-10-05 16:00:01 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer text-center">
+       <div class="wrapper">
+       <marquee><p>2022 All right reserved, Foot House. Developed By -<a href="">IT TEAM</a></p></marquee>
+        </div>
+       </div>
+       <!-- Footer Section Ends -->
+
+    </body>
+</html>
