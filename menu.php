f563cc55dc32bb8c5b06331c596777c7a923c1af
NhorkSopheakna <99951792+NhorkSopheakna@users.noreply.github.com>
2022-10-05 16:00:16 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,30 @@
+<?php 
+        include('../config/constants.php');
+?>
+
+<?php
+        include('../admin/login-check.php'); 
+?>
+
+<html>
+    <header>
+        <title>Restaurant Website</title>
+    <link rel="stylesheet" href="../css/index.css">
+    </header>
+
+<!-- Menu Section Starts -->
+<body>
+    <div >
+        <div class="menu text-center">
+            
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="admin.php">Admin</a></li>
+                <li><a href="category.php">Category</a></li>
+                <li><a href="manage-food.php">Food</a></li>
+                <li><a href="manage-order.php">Order</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+       </div>
+<!-- Menu Section Ends -->
