7491383fac31d2a311f33465688360b8679c476a
NhorkSopheakna <99951792+NhorkSopheakna@users.noreply.github.com>
2022-10-05 16:08:51 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php include('partials/menu.php') ?>
+
+<!-- Body Section Starts -->
+
+
+    <div class="Main-Content">
+        <div class="wrapper">
+            <h1>Food Menu</h1>
+            <br><br>
+
+            <div class="col-4 text-center">
+                <h1>1</h1>
+                Categoriey
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>2</h1>
+                Categoriey
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>3</h1>
+                Categoriey
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>4</h1>
+                Categoriey
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                Categoriey
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>6</h1>
+                Categoriey
+            </div>
+            
+            <div class="col-4 text-center">
+                <h1>7</h1>
+                Categoriey
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>8</h1>
+                Categoriey
+            </div>
+
+            
+            <div class="clearfix"></div>
+    </div>
+    </div>
+        <br>
+        
+<!-- Body Section Ends -->
+<hr>
+
+<br>
+<?php include('partials/footer.php') ?>
