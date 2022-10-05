8bed3883dce6332326c468776b08f366486beb54
NhorkSopheakna <99951792+NhorkSopheakna@users.noreply.github.com>
2022-10-05 17:14:39 +0700

    Update add-admin.php

--- add-admin.php
+++ add-admin.php
@@ -8,6 +8,14 @@ include "fooddb.php";
     <div class="wrapper">
         <h1> Add Admin</h1>
         <br><br>
+        <?php 
+            if(isset($_SESSION['exit']))
+                {
+                    echo $_SESSION['exit'];//Displaying Session Message
+                    unset ($_SESSION['exit']);//Removing Session Message 
+                }
+        ?>
+
         <form action="" method="POST">
         <table class="tbl-30">
             <tr>
@@ -49,19 +57,30 @@ include "fooddb.php";
 </body>
 
 <?php
+    
+    $username="";
+    if(isset($_POST["submit"]))
+    {
+        $username = $_POST['username'];
+        $sql_u = "SELECT * FROM tbl_admin WHERE username = '$username'";
+        $res_u =mysqli_query( $link, $sql_u);
 
-if(isset($_POST["submit"]))
-{
-    //echo "Click!";
-	mysqli_query($link,"insert into tbl_admin values(NULL,'$_POST[full_name]','$_POST[username]',md5('$_POST[password]'))");
-	?>
-	<script type="text/javascript">
-    window.location="admin.php";
-    </script>
-    <?php
-}
+        if (mysqli_num_rows($res_u) ==1) {
+            $_SESSION['exit'] = "<div class='error'>User exit. Try Again with other username.</div>";
+            //Redirect to Manage Category
+            header('location:'.SITEURL.'admin/add-admin.php'); 
+        
+        //echo "Click!";
+        }else
+        {
+            mysqli_query($link,"insert into tbl_admin values(NULL,'$_POST[full_name]','$_POST[username]',md5('$_POST[password]'))");
+            ?>
+            <script type="text/javascript">
+            window.location="admin.php";
+            </script>
 
+            <?php
+        }
+    }
 
 ?>
-
-
